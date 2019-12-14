8ac744dd31b8e5a5c38d50a24baeb2c9c9a0ab84
Nora <noraniederer1@Noras-MacBook-Air.local>
2019-12-14 12:45:33 +0100

    commit - new back to top btn

--- scripts/main.js
+++ scripts/main.js
@@ -41,3 +41,26 @@ function Function1() {
 function Function2() {
   document.getElementById("myDropdown2").classList.toggle("show");
 }
+
+// Back to top button
+//Get the button
+var myTopButton = document.getElementById("myTopBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    myTopButton.style.display = "block";
+  } else {
+    myTopButton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
